2e769b7ef351b833f7be405635633078651e976d
Anya <anya.p.nguyen@gmail.com>
2018-02-26 11:07:20 -0800

    moved to local vars in store.js

--- scripts/Item.js
+++ scripts/Item.js
@@ -1,21 +1,22 @@
+/*global cuid */
 'use strict';
 
 const Item = (function() {
 
-	const validateName = function(name) {
-		console.log(name);
-		if (name === "") { throw "Name does not exist."}	
-		// if (typeof name === "undefined") { console.error("Name does not exist.")}
-		// if (typeof name === "undefined") { console.log("Name does not exist.")}	
-	}
+  const validateName = function(name) {
+    console.log(name);
+    if (name === '') throw new TypeError('Name does not exist.');
+    // if (typeof name === "undefined") { console.error("Name does not exist.")}
+    // if (typeof name === "undefined") { console.log("Name does not exist.")}	
+  };
 
-	const create = function(name) {
-		return {
-			id: cuid(),
-			name: name,
-			checked: false
-		}
-	}
+  const create = function(name) {
+    return {
+      id: cuid(),
+      name: name,
+      checked: false
+    };
+  };
 
   return {
     validateName,
--- scripts/store.js
+++ scripts/store.js
@@ -1,20 +1,21 @@
-'use srict';
+/* global cuid */
+'use strict';
 
 const store = (function() {
   
   //   const addItem = function(itemName) {};
   //   const updateItem = function(id, updateData) {};
   //   const toggleHideCheckedFilter = function() {};
-
+  const item = [
+    { id: cuid(), name: 'apples', checked: false },
+    { id: cuid(), name: 'oranges', checked: false },
+    { id: cuid(), name: 'milk', checked: true },
+    { id: cuid(), name: 'bread', checked: false }
+  ];
+  const hideCheckedItems = false;
+  const searchTerm = '';
   return {
-    items: [
-      { id: cuid(), name: 'apples', checked: false },
-      { id: cuid(), name: 'oranges', checked: false },
-      { id: cuid(), name: 'milk', checked: true },
-      { id: cuid(), name: 'bread', checked: false }
-    ],
-    hideCheckedItems: false,
-    searchTerm: '',
+    item, hideCheckedItems, searchTerm
     // addItem, updateItem, toggleHideCheckedFilter
   };
         
